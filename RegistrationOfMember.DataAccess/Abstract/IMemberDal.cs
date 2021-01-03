2f5a320b1d23a8fadc14d2875c18b56d80c095a6
Muhammed Fatih CEYLAN <Yl1930138013@ogr.sdu.edu.tr>
2021-01-03 22:04:09 +0300

    Add project files.

---
+++ RegistrationOfMember.DataAccess/Abstract/IMemberDal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevFramework.Core.DataAccess;
+using RegistrationOfMember.Entities.Concrete;
+
+namespace RegistrationOfMember.DataAccess.Abstract
+{
+    public interface IMemberDal:IEntityRepository<Member>
+    {
+    }
+}
---
+++ RegistrationOfMember.DataAccess/Concrete/EntityFramework/MembershipContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RegistrationOfMember.DataAccess.Concrete.EntityFramework.Mapping;
+using RegistrationOfMember.Entities.Concrete;
+
+namespace RegistrationOfMember.DataAccess.Concrete.EntityFramework
+{
+    public class MembershipContext:DbContext
+    {
+        public MembershipContext()
+        {
+            Database.SetInitializer<MembershipContext>(null);
+        }
+
+        public DbSet<Member> Members { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new MemberMap());
+        }
+    }
+}
